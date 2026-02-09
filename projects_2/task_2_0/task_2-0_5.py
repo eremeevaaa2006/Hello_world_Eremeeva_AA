3e4d66cbc30efd641639b376b81e8ddb8b8b2786
Еремеева Анна Александровна <eremeeva_aa06@mail.ru>
2026-02-09 22:57:02 +0300

    Add files via upload

---
+++ projects_2/task_2_0/task_2-0_5.py
@@ -0,0 +1,4 @@
+name = "Анна"
+group = 50002
+score = 100
+print(f"Студентка {name} из группы {group} получила {score} баллов.")
---
+++ projects_2/task_2_0/task_2-0_6.py
@@ -0,0 +1,3 @@
+f = open("C:/Users/Anna/Desktop/eremeeva_aa/projects_2/task_2_0/output.txt", "w", encoding="utf-8")
+print("Еремеева Анна учится в СПбПУ Петра Великого", file=f)
+f.close()
