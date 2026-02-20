55575636969715aa472ea8112d4254f1c4f46213
Еремеева Анна Александровна <eremeeva_aa06@mail.ru>
2026-02-20 11:45:07 +0300

    Add files via upload

---
+++ projects_2/task_2_7/task_2-7_1.py
@@ -0,0 +1,5 @@
+files = ["seq1", "seq2", "seq3", "seq4"]
+
+for name in files:
+   new_name = name + ".fasta" + " 02.02.2022"
+   print(f"{new_name}")
---
+++ projects_2/task_2_7/task_2-7_2.py
@@ -0,0 +1,11 @@
+seq = [ "ATATACGCGTA", "CTTCGGNGGA" ]
+
+print(seq[0])
+for n in seq[0]:
+    print(n)
+
+print(seq[1])
+for n in seq[1]:
+    print(n)
+
+print("Цикл выполнен")
