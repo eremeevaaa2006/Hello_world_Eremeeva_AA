fecf8ed59168e919001a3c0ab125b07573cf4de8
Еремеева Анна Александровна <eremeeva_aa06@mail.ru>
2026-03-04 20:26:51 +0300

    Add files via upload

---
+++ projects_3/task_3_4/task_3-4_1_2.py
@@ -0,0 +1,30 @@
+ls -lah /
+
+итого 76K
+drwxr-xr-x  19 root root 4,0K фев 20 12:34 .
+drwxr-xr-x  19 root root 4,0K фев 20 12:34 ..
+lrwxrwxrwx   1 root root    7 фев 20 12:01 bin -> usr/bin
+drwxr-xr-x   3 root root 4,0K фев 27 13:14 boot
+drwx------   2 root root 4,0K фев 20 12:34 .cache
+drwxr-xr-x  19 root root 3,2K фев 27 13:20 dev
+drwxr-xr-x 123 root root  12K фев 27 13:20 etc
+drwxr-xr-x   4 root root 4,0K фев 27 13:14 home
+lrwxrwxrwx   1 root root   35 фев 20 12:09 initrd.img -> boot/initrd.img-6.12.73+deb13-amd64
+lrwxrwxrwx   1 root root   35 фев 20 12:06 initrd.img.old -> boot/initrd.img-6.12.63+deb13-amd64
+lrwxrwxrwx   1 root root    7 фев 20 12:01 lib -> usr/lib
+lrwxrwxrwx   1 root root    9 фев 20 12:01 lib64 -> usr/lib64
+drwx------   2 root root  16K фев 20 12:00 lost+found
+drwxr-xr-x   3 root root 4,0K фев 20 12:00 media
+drwxr-xr-x   2 root root 4,0K фев 20 12:01 mnt
+drwxr-xr-x   3 root root 4,0K фев 27 13:13 opt
+dr-xr-xr-x 294 root root    0 фев 27 13:19 proc
+drwx------   4 root root 4,0K фев 27 12:34 root
+drwxr-xr-x  27 root root  680 фев 27 13:20 run
+lrwxrwxrwx   1 root root    8 фев 20 12:01 sbin -> usr/sbin
+drwxr-xr-x   2 root root 4,0K фев 20 12:01 srv
+dr-xr-xr-x  13 root root    0 фев 27 13:19 sys
+drwxrwxrwt  15 root root  360 фев 27 13:30 tmp
+drwxr-xr-x  12 root root 4,0K фев 20 12:01 usr
+drwxr-xr-x  11 root root 4,0K фев 20 12:45 var
+lrwxrwxrwx   1 root root   32 фев 20 12:09 vmlinuz -> boot/vmlinuz-6.12.73+deb13-amd64
+lrwxrwxrwx   1 root root   32 фев 20 12:06 vmlinuz.old -> boot/vmlinuz-6.12.63+deb13-amd64
