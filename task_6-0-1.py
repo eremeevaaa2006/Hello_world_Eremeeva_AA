3aa23ee39220a3fa07ec6ad7436710aaa8981487
Еремеева Анна Александровна <eremeeva_aa06@mail.ru>
2026-05-02 21:51:46 +0300

    Delete task_6-0-1.py

--- task_6-0-1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv("C:/Users/Anna/Desktop/eremeeva_aa/projects_6/wild_boars.csv")
-
-min_tusk = df['tusk_length_cm'].min()
-max_tusk = df['tusk_length_cm'].max()
-
-print(f"Самые короткие клыки: {min_tusk} см")
-print(f"Самые длинные клыки: {max_tusk} см")
