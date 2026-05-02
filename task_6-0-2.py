c1f6a8b9e0d75b78d17f9c4f3c5f2c17ff464088
Еремеева Анна Александровна <eremeeva_aa06@mail.ru>
2026-05-02 21:51:38 +0300

    Delete task_6-0-2.py

--- task_6-0-2.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv("C:/Users/Anna/Desktop/eremeeva_aa/projects_6/wild_boars.csv")
-columns = ['age_years', 'weight_kg', 'length_cm', 'shoulder_height_cm', 'tusk_length_cm', 'litter_size', 'health_score', 'territory_ha']
-mean_values = df[columns].mean()
-
-with open("C:/Users/Anna/Desktop/eremeeva_aa/projects_6/mean_values.txt", "w", encoding="utf-8") as file:
-    for parametr, value in mean_values.items():
-        file.write(f"{parametr}: {value:.2f}\n")
